6aec3677284a364beeb7f95b972fe911cac794fd
Ivan Shires <60198379+IvanShires@users.noreply.github.com>
2022-11-26 21:52:19 -0500

    refactored everything to make it more dynamic

--- excavate_alt.lua
+++ excavate_alt.lua
@@ -1,3 +1,5 @@
+good_items = {"diamond","gold","lapis","iron","emerald","uranium","obsidian","coal","redstone","torch","copper","tin","ore"}
+
 print("Checking for updated script..")
 paste_id = "http://pastebin.com/raw.php?i=ranQP0a1"
 pasteContent = http.get(paste_id)
@@ -19,6 +21,14 @@ end
 
 function ntfy_message(ore)
     local response = http.post("https://ntfy.sh/ishires_smartMiner",os.getComputerID() .. " - Just mined a "..ore)
+end
+
+function string_contains(string,good_items)
+    for key,value in pairs(good_items) do
+        if string.match(value, string) then
+            return "True"
+        end
+    end
 end
 
 function check_block()
@@ -59,19 +69,23 @@ function check_block()
     end
 end
 
-function turtle_asend()
+function turtle_UpInspect()
     local block, block_data = turtle.inspectUp()
     if block then -- Block is detected by the Turtle
         local block_name = block_data["name"]
         if (string.find(block_name,"obsidian")) then
-            turtle.dig()
-            turtle.up()
+            return "True"
         else
-            mining = False
+            return "False"
         end
     else
-        turtle.up()
+        return "True"
     end
+end
+
+function turtle_asend()
+    turtle.digUp()
+    turtle.up()
 end
 
 function bedrock_check()
@@ -79,22 +93,43 @@ function bedrock_check()
     if block then -- Block is detected by the Turtle
         local block_name = block_data["name"]
         if (string.find(block_name,"bedrock")) then
-            while true do
-                turtle_asend()
-            end
+            return "True"
         else
-            turtle.digDown()
-            turtle.down()
+            return "False"
         end
     else
+        return "False"
+    end
+end
+mining = "True"
+while mining == "True" do
+    if (bedrock_check() == "True") then -- We've hit bedrock, go back up...
+        while true do
+            if (turtle_UpInspect() == "False") then
+                mining = "False"
+                break
+            else
+                turtle_asend()
+            end
+        end
+    else
+        for i=1,4 do -- checks all 4 sides of the Turtle
+            check_block()
+            turtle.turnRight()
+        end
+        turtle.digDown()
         turtle.down()
     end
+    
 end
-mining = True
-while mining == True do
-    bedrock_check()
-    for i=1,4 do -- checks all 4 sides of the Turtle
-        check_block()
-        turtle.turnRight()
+-- Inventory Check
+for i = 1, 16 do
+    turtle.select(i)
+    block_data = turtle.getItemDetail()
+    block_name = block_data["name"]
+    if string_contains(block_name,good_items) then
+        print("yay")
+    else
+        print("nope")
     end
-end
+  end
